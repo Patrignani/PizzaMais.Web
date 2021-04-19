9e1288f9217eef98273373fe321ad04730c60bfc
andersonpatrignani <anderson.patrignani@gmail.com>
2021-04-19 08:36:19 -0300

    no message

--- src/utils/templateCompilation/form.js
+++ src/utils/templateCompilation/form.js
@@ -2,36 +2,71 @@
 const formTemplate = `<div class="body">
 <b-card>
   <b-form>
-    $$row$$
-    <div class="buttons">
-      $$buttons$$
-    </div>
+    $$rows$$
+    <ButtonsForm :configButtons="configButtons" />
   </b-form>
 </b-card>
-</div>`
-
-const buttonTemplate = `    <b-button size="lg" variant="primary">Cancelar</b-button>
-<b-button size="lg" variant="primary" @click="save">Salvar</b-button>`
+</div> `
 
 const rowTemplate = `  <b-row class="body-col" id="$$rowId$$">
     $$column$$
-</b-row>`
+</b-row> `
 
-const columnTemplate = `  <b-col :sm="column.col" id="$$colId$$">
+const columnTemplate = `  <b-col :sm="$$col$$" id="$$colId$$">
     $$component$$
-</b-col>`
+</b-col> `
 
+const createComponent = (propsColumn) => {
+  let component = " ";
+  switch (propsColumn.componentType) {
+    case "input":
+      component += `<Input
+        labelGroupName="${propsColumn.component.labelGroupName}"
+        inputId="${propsColumn.component.inputId}"
+        :value="column.component.value"
+        v-on:input="column.component.value = $event"
+        $$disabled$$
+
+        $$validate$$
+
+      />`
+
+      component = component.replace("$$disabled$$", propsColumn.component.disabled != undefined ? propsColumn.component.disabled : false)
+
+      if (propsColumn.component.validate) {
+        component = component.replace("$$validate$$", ` :validate="{
+          using: column.component.validate.using,
+          state: column.component.validate.state,
+          message: column.component.validate.message,
+        }" `)
+      }
+      else {
+        component = component.replace("$$validate$$", "")
+      }
+
+      break;
+  }
+
+  return component
+}
 
 export const createForm = (rowsParam) => {
-    let form = formTemplate
-    let rows = " "
-    rowsParam.forEach(rowParam => {
-        let row = rowTemplate.replace('$$rowId$$', row.id)
+  let rows = " "
+  rowsParam.forEach(rowParam => {
+    let row = rowTemplate.replace('$$rowId$$', row.id)
+    let columns = " "
+    rowParam.columns.forEach(columnParam => {
+      let column = columnTemplate.replace("$$col$$", columnParam.col)
+        .replace("$$colId$$", columnParam.id)
 
-        rowParam.columns.forEach(columnParam => {
-            // let column = 
-        })
+        column = column.replace("$$component$$", createComponent(columnParam))
 
-        rows += row
-    });
+      columns += column
+    })
+
+    row = row.replace("$$column$$", columns)
+    rows += row
+  });
+
+  formTemplate.replace("$$rows$$",rows)
 }
